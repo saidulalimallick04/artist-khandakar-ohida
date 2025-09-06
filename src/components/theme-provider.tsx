d8e49c32ca87a4cef90780dfdd3c48b8a9890968
Safiya Begum <safiya4begum06@gmail.com>
2025-09-06 06:13:46 +0000

    find any depricated module/package/component is used or not?

--- src/components/theme-provider.tsx
+++ src/components/theme-provider.tsx
@@ -1,80 +1,9 @@
-
 "use client"
 
 import * as React from "react"
+import { ThemeProvider as NextThemesProvider } from "next-themes"
+import type { ThemeProviderProps } from "next-themes/dist/types"
 
-type Theme = "dark" | "light"
-
-type ThemeProviderProps = {
-  children: React.ReactNode
-  defaultTheme?: Theme
-  storageKey?: string
-  enableSystem?: boolean,
-  attribute?: 'class'
-}
-
-type ThemeProviderState = {
-  theme: Theme
-  setTheme: (theme: Theme) => void
-}
-
-const initialState: ThemeProviderState = {
-  theme: "dark",
-  setTheme: () => null,
-}
-
-const ThemeProviderContext = React.createContext<ThemeProviderState>(initialState)
-
-export function ThemeProvider({
-  children,
-  defaultTheme = "dark",
-  storageKey = "ui-theme",
-  enableSystem = false,
-  ...props
-}: ThemeProviderProps) {
-  const [theme, setTheme] = React.useState<Theme>(() => {
-    if (typeof window === "undefined") {
-      return defaultTheme
-    }
-    return (localStorage.getItem(storageKey) as Theme) || defaultTheme
-  })
-
-  React.useEffect(() => {
-    const root = window.document.documentElement
-    
-    root.classList.remove("light", "dark")
-
-    let effectiveTheme = theme;
-    if (theme === "system" && enableSystem) {
-      const systemTheme = window.matchMedia("(prefers-color-scheme: dark)").matches ? "dark" : "light"
-      effectiveTheme = systemTheme;
-    }
-
-    root.classList.add(effectiveTheme)
-  }, [theme, enableSystem])
-
-  const value = {
-    theme,
-    setTheme: (theme: Theme) => {
-      if (typeof window !== "undefined") {
-        localStorage.setItem(storageKey, theme)
-      }
-      setTheme(theme)
-    },
-  }
-
-  return (
-    <ThemeProviderContext.Provider {...props} value={value}>
-      {children}
-    </ThemeProviderContext.Provider>
-  )
-}
-
-export const useTheme = () => {
-  const context = React.useContext(ThemeProviderContext)
-
-  if (context === undefined)
-    throw new Error("useTheme must be used within a ThemeProvider")
-
-  return context
+export function ThemeProvider({ children, ...props }: ThemeProviderProps) {
+  return <NextThemesProvider {...props}>{children}</NextThemesProvider>
 }
