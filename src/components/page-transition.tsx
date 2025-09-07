daa03106499956b19cd8768a38528ade13a81826
Safiya Begum <safiya4begum06@gmail.com>
2025-09-07 04:41:33 +0000

    revert changes

--- src/components/page-transition.tsx
+++ src/components/page-transition.tsx
@@ -1,20 +1,8 @@
 
 'use client';
 
-import { AnimatePresence, motion } from 'framer-motion';
-import { usePathname } from 'next/navigation';
 import type { ReactNode } from 'react';
-import { Stairs } from './stairs';
 
 export const PageTransition = ({ children }: { children: ReactNode }) => {
-    const pathname = usePathname();
-  return (
-    <AnimatePresence mode="wait">
-        <div key={pathname}>
-            <div className="fixed top-0 left-0 w-screen h-screen bg-primary pointer-events-none z-[100]" />
-            <Stairs />
-            {children}
-        </div>
-    </AnimatePresence>
-  );
+  return <>{children}</>;
 };
